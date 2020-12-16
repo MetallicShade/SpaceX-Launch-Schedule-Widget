923f886d33895183a470fcd6e09b0c9029a788aa
MetallicShade <67353333+MetallicShade@users.noreply.github.com>
2020-12-16 13:57:44 -0600

    Update SpaceX-Launch-Schedule-Widget.js

--- SpaceX-Launch-Schedule-Widget.js
+++ SpaceX-Launch-Schedule-Widget.js
@@ -2,7 +2,7 @@ const url="https://spacelaunchschedule.com/spacex-launch-schedule/"
 const req=new Request(url)
 var data=await req.loadString()
 var rocket=data.substring(data.indexOf("<h4 class=\"card-title h5\"")+97,data.indexOf("</h4>")-1)
-var date=data.substring(data.indexOf("<time datetime=\"")+16,data.indexOf("\">"),data.indexOf("<time datetime=\"")+16))
+var date=data.substring(data.indexOf("<time datetime=\"")+16,data.indexOf("\">"),data.indexOf("<time datetime=\"")+16)
 var location=data.substring(data.indexOf("<div class=\"col truncate")+27,data.indexOf("</div>",data.indexOf("<div class=\"col truncate")+27))
 const testAlert=new Alert()
 testAlert.title=location
