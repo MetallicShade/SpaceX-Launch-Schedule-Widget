c108d6f78fc7df718772eabbf1b9eefada593f32
MetallicShade <67353333+MetallicShade@users.noreply.github.com>
2020-12-17 11:36:45 -0600

    Revert "Icons"
    This reverts commit 3bd88226c0fae2895acb735c3e036f02ec918db9.

--- SpaceX-Launch-Schedule-Widget.js
+++ SpaceX-Launch-Schedule-Widget.js
@@ -1,23 +1,14 @@
 const url="https://ll.thespacedevs.com/2.0.0/launch/upcoming/?mode=list&search=SpaceX"
 const req=new Request(url)
 let api=await req.loadJSON()
-<<<<<<< Updated upstream
 let name=api.results[0].name
 let date=new Date(api.results[0].net)
 let location=api.results[0].location
-=======
-var rocket=api.results[0].name
-var date=new Date(api.results[0].net)
-var location=api.results[0].location
-var fm=Filemanager.iCloud()
-var scriptablePath = "/var/mobile/Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents/"
->>>>>>> Stashed changes
 const widget = await createWidget()
 Script.setWidget(widget)
 Script.complete()
 async function createWidget() {
   let widget=new ListWidget()
-<<<<<<< Updated upstream
   w.backgroundcolor="#000000"
   let name = widget.addStack()
   name.addText("name")
@@ -30,21 +21,4 @@ async function createWidget() {
   let countDown=widget.addDate(date)
   countDown.applyTimerStyle()
   return w
-=======
-  widget.backgroundcolor="#000000"
-  let testDate=new Date(2020,11,18,0,0,0,0)
-  addItem(widget,"rocket","rocket")
-  addItem(widget,"date","date")
-  addItem(widget,"location","location")
-  let timer = widget.addStack()
-  timer.addImage(fm.readImage(scriptablePath+"timer.PNG"))
-  let countDown=timer.addDate(testDate)
-  countDown.applyTimerStyle()
-  return widget
-}
-function addItem(widget, icon, text) {
-  let this.item=widget.addStack()
-  item.addImage(fm.readImage(scriptablePath+icon+".PNG"))
-  item.addText(text)
->>>>>>> Stashed changes
 }
