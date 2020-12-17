972d0dc03b84c9de8e4ec361c1f27e8116de2455
MetallicShade <67353333+MetallicShade@users.noreply.github.com>
2020-12-17 09:18:02 -0600

    Update SpaceX-Launch-Schedule-Widget.js

--- SpaceX-Launch-Schedule-Widget.js
+++ SpaceX-Launch-Schedule-Widget.js
@@ -1,18 +1,21 @@
 const url="https://ll.thespacedevs.com/2.0.0/launch/upcoming/?mode=list&search=SpaceX"
 const req=new Request(url)
-var data=await req.loadJSON()
-var name=data.results[0].name
-var date=data.results[0].window_start
-var location=data.results[0].location
-console.log(name)
-console.log(date)
-console.log(loacation)
+let api=await req.loadJSON()
+let name=api.results[0].name
+let date=new Date(api.results[0].net)
+let location=api.results[0].location
 const widget = await createWidget()
 Script.setWidget(widget)
 Script.complete()
 async function createWidget() {
-  const w=new ListWidget()
+  let widget=new ListWidget()
   w.backgroundcolor="#000000"
+  let name = widget.addStack()
+  name.addText("name")
+  let date = widget.addStack()
+  date.addText("date")
+  let location - widget.addStack()
+  location.addText("location")
   //w.addDate()
   //let date=new Date(2020,12,16,22,10,0,0)
   //let countDown=w.addDate(date)
