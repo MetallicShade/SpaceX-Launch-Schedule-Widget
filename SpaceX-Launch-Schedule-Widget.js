47e4582d4cd5cbdb9a7153cd5aa09bca83be56d3
MetallicShade <67353333+MetallicShade@users.noreply.github.com>
2020-12-25 08:33:13 -0600

    Update SpaceX-Launch-Schedule-Widget.js

--- SpaceX-Launch-Schedule-Widget.js
+++ SpaceX-Launch-Schedule-Widget.js
@@ -1,39 +1,38 @@
-const url="https://ll.thespacedevs.com/2.0.0/launch/upcoming/?mode=list&search=SpaceX"
-const req=new Request(url)
-let api=await req.loadJSON()
-var rocket=api.results[0].name
-var date=new Date(api.results[0].net)
-date.toLocaleString()
-var location=api.results[0].location
-var fm=FileManager.iCloud()
-var scriptablePath = "/var/mobile/Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents/"
+const url = "https://ll.thespacedevs.com/2.0.0/launch/upcoming/?mode=list&search=SpaceX"
+const req = new Request(url)
+let api = await req.loadJSON()
+var rocket = api.results[0].name
+var date = new Date(api.results[0].net).toLocaleString()
+var location = api.results[0].location
 const widget = await createWidget()
 Script.setWidget(widget)
 Script.complete()
 async function createWidget() {
-  let widget=new ListWidget()
-  widget.backgroundcolor=Color.black()
-  let testDate=new Date(2020,11,18,0,0,0,0)
-  addItem(widget,rocket,null,false)
-  addItem(widget,date.toLocaleString(),"calendar",false)
-  addItem(widget,location,"mappin.and.ellipse",false)
-  addItem(widget,date,"timer",true)
-  let timer = widget.addStack()
-  timer.addImage(fm.readImage(scriptablePath+"timer.PNG"))
-  let countDown=timer.addDate(testDate)
-  countDown.applyTimerStyle()
+  let widget = new ListWidget()
+  widget.backgroundcolor = Color.black()
+  widget.spacing = 8
+  addItem(widget, rocket, null, false)
+  addItem(widget, date, "calendar", false)
+  addItem(widget, location, "mappin.and.ellipse", false)
+  addItem(widget, date, "timer", true)
   return widget
 }
+
 function addItem(widget, text, symbol, timer) {
-  let item=widget.addStack()
-  if (symbol!=null) {
-    let icn=item.addImage(SFSymbol.named(symbol).image)
-    icn.tintColor=Color.white()
+  let item = widget.addStack()
+  if (symbol != null) {
+    let icn = item.addImage(SFSymbol.named(symbol).image)
+    icn.imageSize = new Size(22, 22)
+    icn.tintColor = Color.white()
+    item.addSpacer()
   }
-  if (timer=true) {
-    let countDown=item.addDate(text)
-  }
-  else if (text!=null) {
-    item.addText(text)
+  if (timer == true) {
+    let countDown = item.addDate(new Date(text))
+    countDown.applyTimerStyle()
+    countDown.rightAlignText()
+    countDown.font = Font.mediumSystemFont(10)
+  } else if (text != null) {
+    let t = item.addText(text)
+    t.font = Font.mediumSystemFont(10)
   }
 }
